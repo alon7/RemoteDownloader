654a934599f15f257798fb26c4aa611a8c9ee0c9
Max Levin <maxter161@gmail.com>
2014-03-01 17:34:04 +0200

    first commit

---
+++ subtitles/subtitle.py
@@ -0,0 +1,236 @@
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import urllib
+import shutil
+from subtitles.urlHandler import URLHandler
+
+
+class SubtitleHelper:
+    BASE_URL = "http://www.subtitle.co.il"
+
+    def __init__(self):
+        self.urlHandler = URLHandler()
+
+    def get_subtitle_list(self, item):
+        if item["tvshow"]:
+            search_results = self._search_tvshow(item)
+            results = self._build_tvshow_subtitle_list(search_results, item)
+        else:
+            search_results = self._search_movie(item)
+            results = self._build_movie_subtitle_list(search_results, item)
+
+        return results
+
+    # return list of tv-series from the site`s search
+    def _search_tvshow(self, item):
+        search_string = re.split(r'\s\(\w+\)$', item["tvshow"])[0]
+
+        cache_key = get_cache_key("tv-show", search_string)
+        results = cache.get(cache_key)
+
+        if not results:
+            query = {"q": search_string.lower(), "cs": "series"}
+
+            search_result = self.urlHandler.request(self.BASE_URL + "/browse.php?" + urllib.urlencode(query))
+            if search_result is None:
+                return results  # return empty set
+
+            urls = re.findall(
+                u'<a href="viewseries\.php\?id=(\d+)[^"]+" itemprop="url">[^<]+</a></div><div style="direction:ltr;" class="smtext">([^<]+)</div>',
+                search_result)
+
+            results = self._filter_urls(urls, search_string, item)
+            if results:
+                cache.set(cache_key, repr(results))
+        else:
+            results = eval(results)
+
+        return results
+
+    # return list of movie from the site`s search
+    def _search_movie(self, item):
+        results = []
+        search_string = item["title"]
+        query = {"q": search_string.lower(), "cs": "movies"}
+        if item["year"]:
+            query["fy"] = int(item["year"]) - 1
+            query["uy"] = int(item["year"]) + 1
+
+        search_result = self.urlHandler.request(self.BASE_URL + "/browse.php?" + urllib.urlencode(query))
+        if search_result is None:
+            return results  # return empty set
+
+        urls = re.findall(
+            u'<a href="view\.php\?id=(\d+)[^"]+" itemprop="url">[^<]+</a></div><div style="direction:ltr;" class="smtext">([^<]+)</div><span class="smtext">(\d{4})</span>',
+            search_result)
+
+        results = self._filter_urls(urls, search_string, item)
+        return results
+
+    def _filter_urls(self, urls, search_string, item):
+        filtered = []
+        search_string = search_string.lower()
+
+        if not item["tvshow"]:
+            for (id, eng_name, year) in urls:
+                if search_string.startswith(eng_name.lower()) and \
+                        (item["year"] == '' or
+                                 year == '' or
+                                     (int(year) - 1) <= int(item["year"]) <= (int(year) + 1) or
+                                     (int(item["year"]) - 1) <= int(year) <= (int(item["year"]) + 1)):
+                    filtered.append({"name": eng_name, "id": id, "year": year})
+        else:
+            for (id, eng_name) in urls:
+                if search_string.startswith(eng_name.lower()):
+                    filtered.append({"name": eng_name, "id": id})
+
+        log(__scriptname__, "filtered: %s" % filtered)
+        return filtered
+
+    def _build_movie_subtitle_list(self, search_results, item):
+        ret = []
+        total_downloads = 0
+        for result in search_results:
+            url = self.BASE_URL + "/view.php?" + urllib.urlencode({"id": result["id"], "m": "subtitles"})
+            subtitle_page = self._is_logged_in(url)
+            x, i = self._retrive_subtitles(subtitle_page, item)
+            total_downloads += i
+            ret += x
+
+        # Fix the rating
+        if total_downloads:
+            for it in ret:
+                it["rating"] = str(int(round(it["rating"] / float(total_downloads), 1) * 5))
+
+        return sorted(ret, key=lambda x: (x['lang_index'], x['sync'], x['rating']), reverse=True)
+
+    def _build_tvshow_subtitle_list(self, search_results, item):
+        ret = []
+        total_downloads = 0
+
+        for result in search_results:
+            cache_key_season = get_cache_key("tv-show", "%s_%s" % (result["name"], "seasons"))
+            subtitle_page = cache.get(cache_key_season)
+            if not subtitle_page:
+                used_cached_seasons = False
+                url = self.BASE_URL + "/viewseries.php?" + urllib.urlencode({"id": result["id"], "m": "subtitles"})
+                subtitle_page = self._is_logged_in(url)
+                if subtitle_page is not None:
+                    # Retrieve the requested season
+                    subtitle_page = re.findall("seasonlink_(\d+)[^>]+>(\d+)</a>", subtitle_page)
+                    if subtitle_page:
+                        cache.set(cache_key_season, repr(subtitle_page))
+            else:
+                used_cached_seasons = True
+                subtitle_page = eval(subtitle_page)
+
+            if subtitle_page:
+                season_found = False
+                for (season_id, season_num) in subtitle_page:
+                    if season_num == item["season"]:
+                        season_found = True
+
+                        cache_key_episode = get_cache_key("tv-show",
+                                                          "%s_s%s_%s" % (result["name"], season_num, "episodes"))
+                        found_episodes = cache.get(cache_key_episode)
+                        if not found_episodes:
+                            used_cached_episodes = False
+                            # Retrieve the requested episode
+                            url = self.BASE_URL + "/getajax.php?" + urllib.urlencode({"seasonid": season_id})
+                            subtitle_page = self.urlHandler.request(url)
+                            if subtitle_page is not None:
+                                found_episodes = re.findall("episodelink_(\d+)[^>]+>(\d+)</a>", subtitle_page)
+
+                                if found_episodes:
+                                    cache.set(cache_key_episode, repr(found_episodes))
+                        else:
+                            used_cached_episodes = True
+                            found_episodes = eval(found_episodes)
+
+                        if found_episodes:
+                            episode_found = False
+                            for (episode_id, episode_num) in found_episodes:
+                                if episode_num == item["episode"]:
+                                    episode_found = True
+
+                                    url = self.BASE_URL + "/getajax.php?" + urllib.urlencode(
+                                        {"episodedetails": episode_id})
+                                    subtitle_page = self.urlHandler.request(url)
+
+                                    x, i = self._retrive_subtitles(subtitle_page, item)
+                                    total_downloads += i
+                                    ret += x
+
+                            if not episode_found and used_cached_episodes:
+                                cache.delete(cache_key_episode)  # used cached episodes list and not found
+                                return self._build_tvshow_subtitle_list(search_results, item)  # try the search again
+
+                if not season_found and used_cached_seasons:
+                    cache.delete(cache_key_season)  # used cached season list and not found so delete the cache
+                    return self._build_tvshow_subtitle_list(search_results, item)  # try the search again
+
+        # Fix the rating
+        if total_downloads:
+            for it in ret:
+                it["rating"] = str(int(round(it["rating"] / float(total_downloads), 1) * 5))
+
+        return sorted(ret, key=lambda x: (x['lang_index'], x['sync'], x['rating']), reverse=True)
+
+    def _retrive_subtitles(self, page, item):
+        ret = []
+        total_downloads = 0
+        if page is not None:
+            found_subtitles = re.findall(
+                "downloadsubtitle\.php\?id=(?P<fid>\d*).*?subt_lang.*?title=\"(?P<language>.*?)\".*?subtitle_title.*?title=\"(?P<title>.*?)\">.*?>(?P<downloads>[^ ]+) הורדות",
+                page)
+            for (subtitle_id, language, title, downloads) in found_subtitles:
+                if xbmc.convertLanguage(heb_to_eng(language), xbmc.ISO_639_2) in item["3let_language"]:
+                    subtitle_rate = self._calc_rating(title, item["file_original_path"])
+                    total_downloads += int(downloads.replace(",", ""))
+                    ret.append(
+                        {'lang_index': item["3let_language"].index(
+                            xbmc.convertLanguage(heb_to_eng(language), xbmc.ISO_639_2)),
+                         'filename': title,
+                         'link': subtitle_id,
+                         'language_name': xbmc.convertLanguage(heb_to_eng(language),
+                                                               xbmc.ENGLISH_NAME),
+                         'language_flag': xbmc.convertLanguage(heb_to_eng(language),
+                                                               xbmc.ISO_639_1),
+                         'ID': subtitle_id,
+                         'rating': int(downloads.replace(",", "")),
+                         'sync': subtitle_rate >= 4,
+                         'hearing_imp': 0
+                        })
+        return ret, total_downloads
+
+    def download(self, id, zip_filename):
+        query = {"id": id}
+        url = self.BASE_URL + "/downloadsubtitle.php?" + urllib.urlencode(query)
+        f = self.urlHandler.request(url)
+
+        with open(zip_filename, "wb") as subFile:
+            subFile.write(f)
+        subFile.close()
+
+    def _is_logged_in(self, url):
+        content = self.urlHandler.request(url)
+        if content is not None and re.search(r'friends\.php', content):  #check if logged in
+            return content
+        elif self.login():
+            return self.urlHandler.request(url)
+        else:
+            return None
+
+    def login(self):
+        email = __addon__.getSetting("SUBemail")
+        password = __addon__.getSetting("SUBpassword")
+        query = {'email': email, 'password': password, 'Login': 'התחבר'}
+        content = self.urlHandler.request(self.BASE_URL + "/login.php", query)
+        if re.search(r'<form action="/login\.php"', content):
+            xbmc.executebuiltin((u'Notification(%s,%s)' % (__scriptname__, __language__(32005))).encode('utf-8'))
+            return None
+        else:
+            self.urlHandler.save_cookie()
+            return True
---
+++ subtitles/urlHandler.py
@@ -0,0 +1,53 @@
+import os
+import urllib
+import urllib2
+import zlib
+import cookielib
+
+
+class URLHandler():
+    def __init__(self):
+        self.cookie_filename = os.path.join("cookiejar.txt")  # change to default download path in configuration file
+        self.cookie_jar = cookielib.LWPCookieJar(self.cookie_filename)
+        if os.access(self.cookie_filename, os.F_OK):
+            self.cookie_jar.load()
+
+        self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cookie_jar))
+        self.opener.addheaders = [('Accept-Encoding', 'gzip'),
+                                  ('Accept-Language', 'en-us,en;q=0.5'),
+                                  ('Pragma', 'no-cache'),
+                                  ('Cache-Control', 'no-cache'),
+                                  ('User-Agent',
+                                   'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:16.0) Gecko/20100101 Firefox/16.0')]
+
+    def request(self, url, data=None, decode_zlib=True, ajax=False, referer=None, cookie=None):
+        if data is not None:
+            data = urllib.urlencode(data)
+        if ajax:
+            self.opener.addheaders += [('X-Requested-With', 'XMLHttpRequest')]
+        if referer is not None:
+            self.opener.addheaders += [('Referer', referer)]
+        if cookie is not None:
+            self.opener.addheaders += [('Cookie', cookie)]
+
+        content = None
+        try:
+            response = self.opener.open(url, data)
+            if response.code != 200:
+                content = None
+            else:
+                content = response.read()
+
+            if decode_zlib and response.headers.get('content-encoding', '') == 'gzip':
+                try:
+                    content = zlib.decompress(content, 16 + zlib.MAX_WBITS)
+                except zlib.error:
+                    pass
+
+            response.close()
+        except Exception as e:
+            print "Failed to get url: %s\n%s" % (url, e)
+        return content
+
+    def save_cookie(self):
+        self.cookie_jar.save()
